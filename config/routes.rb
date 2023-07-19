382bda948d731ed595c6759c30b51ea759d8ee79
Egyh <124222570+Egyh@users.noreply.github.com>
2023-07-19 04:02:31 +0000

    add Multilingual function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-   root to: "homes#top"
-   get 'home/about' => 'homes#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+   scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
+     root to: "homes#top"
+     get 'home/about' => 'homes#about'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
