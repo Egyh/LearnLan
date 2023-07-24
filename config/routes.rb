207ee4f5c0f398ea7b0197390bb3798af2ff3432
Egyh <124222570+Egyh@users.noreply.github.com>
2023-07-24 07:44:11 +0000

    update association user and article model

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
    scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
      root to: "homes#top"
      get 'about' => 'homes#about'
-      get 'articles/new'
-      post 'articles' => 'articles#create'   #ここを追記します
-      get 'articles/index' 
-      get 'articles/show'
-      get 'articles/edit'
+     resources :articles, only: [:new,:index,:show,:edit,:create,:destroy,:update]
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
